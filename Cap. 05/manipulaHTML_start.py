790b55e95d7a681812a90ffdc7bbe2634b1d1e0f
Gabriel Oriente <gabriel.oriente@mirumagency.com>
2019-12-08 20:20:15 -0300

    Cap05

--- Cap. 05/manipulaHTML_start.py
+++ Cap. 05/manipulaHTML_start.py
@@ -2,3 +2,31 @@
 # Exemplo de processamento e parse de HTML
 #
 from html.parser import HTMLParser
+
+class meuParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Tag de abertura encontrada!")
+        if attrs.__len__() > 0:
+            for a in attrs:
+                print("  Valores encontrados:  ", a[0], " = ", a[1])
+
+    def handle_endtag(self, tag):
+        print("Tag de fechamento encontrada")
+
+    def handle_comment(self, data):
+        print("Comentario encontrado. ")
+
+    def handle_data(self, data):
+        print("Valores encontrados. ")
+        if data.isspace():
+            print("O valor encontrado é um espaço")
+        else:
+            print("O valor encontrado é: ", data)
+
+def minhaClasse():
+    meuParser1 = meuParser()
+    arquivo = open("//Users//gabrieloriente//Desktop//Python//Cap. 05//exemplohtml.html", "r")
+    dados = arquivo.read()
+    meuParser1.feed(dados)
+
+minhaClasse()
--- Cap. 05/manipulaXML_start.py
+++ Cap. 05/manipulaXML_start.py
@@ -3,3 +3,15 @@
 #
 
 import xml.dom.minidom
+
+def manipulaXML():
+    doc = xml.dom.minidom.parse("//Users//gabrieloriente//Desktop//Python//Cap. 05//exemploXML.xml")
+
+    print("Nome da primeira tag: ", str(doc.firstChild.tagName))
+    primeiroNome = doc.getElementsByTagName("firstname")
+    print("O primeiro nome: ", primeiroNome[0].firstChild.nodeValue)
+
+    for skill in doc.getElementsByTagName("skill"):
+        print("Skill encontrado: ", skill.getAttribute("name"))
+
+manipulaXML()
